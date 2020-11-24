553b52c5872734a930dbd97f7b41159b743d08e3
Pedro Medina <Pedro_Medina@external.mckinsey.com>
2020-11-24 17:13:49 +0100

    Add example 4

--- example_004.py
+++ example_004.py
@@ -6,19 +6,21 @@ from airflow.operators.python_operator import PythonOperator
 import numpy as np
 import pandas as pd
 
+def my_new_print_function(df):
+    print(df)
+    df.describe()
+
 # Wrapper functions
-def my_task_1_wrapper_function():
+def my_function_1():
     df1 = pd.DataFrame({'Alphabet': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
                        'A': [4, 3, 5, 2, 1, 7, 7, 5, 9],
                        'B': [0, 4, 3, 6, 7, 10, 11, 9, 13],
                        'C': [1, 2, 3, 1, 2, 3, 1, 2, 3]})
-    print(df1)
-    df1.describe()
+    my_new_print_function(df1)
 
-def my_task_2_wrapper_function():
+def my_function_2():
     df2 = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
-    print(df2)
-    df2.describe()
+    my_new_print_function(df2)
 
 # Arguments
 args = {
@@ -36,12 +38,12 @@ dag = DAG(
 # Tasks
 my_task_1 = PythonOperator(
     task_id='my_task_1',
-    python_callable=my_task_1_wrapper_function,
+    python_callable=my_function_1,
     dag=dag,
 )
 
 my_task_2 = PythonOperator(
     task_id='my_task_2',
-    python_callable=my_task_2_wrapper_function,
+    python_callable=my_function_2,
     dag=dag,
 )
