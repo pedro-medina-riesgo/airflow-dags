dba3d68476867322e3ddfbc90b5ef63bcfdb4795
Pedro Medina <Pedro_Medina@external.mckinsey.com>
2020-12-17 12:03:20 +0100

    Update example_007.py

--- example_007.py
+++ example_007.py
@@ -1,9 +1,9 @@
-from airflow.models import DAG
-from airflow.utils.dates import days_ago
-from airflow.operators.python_operator import PythonOperator
+from airflow.models import DAG 
+from airflow.utils.dates import days_ago 
+from airflow.operators.python_operator import PythonOperator 
 
 
-# Catalog
+# Catalog code
 from airflow.models import Variable
 catalogs_folder = Variable.get("CATALOGS_FOLDER")
 
@@ -12,26 +12,23 @@ prj = Project()
 prj.init(catalogs_folder + '/example_007')
 
 
-# Wrapper function
-def my_function_007(prj):
-    import warnings
-    warnings.filterwarnings("ignore")
+# Notebook code 
+#!/usr/bin/env python
+# coding: utf-8
 
-    prj.cat.info
+import warnings
+warnings.filterwarnings("ignore")
 
+
+# Wrapper functions 
+def preparation_wrapper_function(prj):
     df = prj.cat.raw.locations.load()
-
-    df.shape
-
     df = df.head(500)
-
     prj.cat.save(data=df, dsid='intermediate.locations')
 
+
+def first_validation_wrapper_function(prj):
     prj.cat.intermediate.locations.load()
-
-    # prj.cat.profile(dsid='intermediate.locations')
-
-    prj.cat.info
 
     validation_rules = {
         'intermediate.locations': {
@@ -57,30 +54,77 @@ def my_function_007(prj):
     }
 
     prj.cat.set_validation_rules(validation_rules)
-
     print(prj.cat.validate('intermediate.locations'))
-
     result = prj.cat.validate('intermediate.locations')
     assert result.passed, str(result)
 
-# Arguments
-args = {
-    'owner': 'Airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-}
 
-# Dag
-dag = DAG(
-    dag_id='example_007',
-    default_args=args,
-    description='A simple DAG',
-)
+def second_validation_wrapper_function(prj):
+    prj.cat.intermediate.locations.load()
 
-# Task
-my_task_007 = PythonOperator(
-    task_id='my_task_007',
-    python_callable=my_function_007,
+    validation_rules = {
+        'intermediate.locations': {
+            'rules': [
+                {
+                    'rtype': 'REQUIRED_COLUMNS_RULE',
+                    'mandatory': 1,
+                    'columns': [
+                        'XCOORDINATE', 'YCOORDINATE', 'LOCATION'
+                    ],
+                    'strict': 0,
+                    'paused': 0
+                }, 
+                {
+                    'rtype': 'MIN_MAX_RULE',
+                    'mandatory': 1,
+                    'column': 'XCOORDINATE',
+                    'min_value': 0,
+                    'max_value': 500
+                }
+            ]
+        }
+    }
+
+    prj.cat.set_validation_rules(validation_rules)
+    print(prj.cat.validate('intermediate.locations'))
+    result = prj.cat.validate('intermediate.locations')
+    assert result.passed, str(result)
+
+
+# Arguments 
+args = { 
+    'owner': 'Airflow', 
+    'depends_on_past': False, 
+    'start_date': days_ago(1), 
+} 
+
+# Dag 
+dag = DAG( 
+    dag_id='example_007', 
+    default_args=args, 
+) 
+
+# Tasks 
+preparation = PythonOperator(
+    task_id='preparation',
+    python_callable=preparation_wrapper_function,
     op_kwargs={'prj': prj},
     dag=dag,
 )
+
+first_validation = PythonOperator(
+    task_id='first_validation',
+    python_callable=first_validation_wrapper_function,
+    op_kwargs={'prj': prj},
+    dag=dag,
+)
+
+second_validation = PythonOperator(
+    task_id='second_validation',
+    python_callable=second_validation_wrapper_function,
+    op_kwargs={'prj': prj},
+    dag=dag,
+)
+
+# Pipelines 
+preparation >> first_validation >> second_validation
